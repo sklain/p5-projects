0f87df2dc9b95c24247ae345e2cf94e587ccf36c
sarah klain <sarahklain@gmail.com>
2022-12-07 14:05:16 +1100

    Trying bounce ball and triangle in javascript

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,49 @@
 function setup() {
-  // put setup code here
-}
 
-function draw() {
-  // put drawing code here
-}
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// Set up ball properties
+var x = 50; // Initial x position of the ball
+var y = 50; // Initial y position of the ball
+var dx = 2; // Initial horizontal velocity of the ball
+var dy = 4; // Initial vertical velocity of the ball
+var radius = 10; // Radius of the ball
+
+// Set up triangle properties
+var triangleX = 100; // Initial x position of the triangle
+var triangleY = 100; // Initial y position of the triangle
+var triangleSize = 50; // Size of the triangle
+var triangleOpacity = 0; // Initial opacity of the triangle
+var triangleFadeIncrement = 0.1; // Amount by which the triangle's opacity changes on each animation frame
+
+function animate() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw the ball
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
+
+  // Update the ball's position
+  x += dx;
+  y += dy;
+
+  // Check for collisions with the walls of the canvas
+  if (x + dx > canvas.width - radius || x + dx < radius) {
+    dx = -dx; // Reverse the ball's horizontal direction
+  }
+  if (y + dy > canvas.height - radius || y + dy < radius) {
+    dy = -dy; // Reverse the ball's vertical direction
+  }
+
+  // Draw the triangle
+  ctx.beginPath();
+  ctx.moveTo(triangleX, triangleY);
+  ctx.lineTo(triangleX +
+  }
+
+  
